383b579d7b2be9f14585b34bccd26e790974eee6
Abdullah F. Khan <abdullahkhan0503@gmail.com>
2019-01-16 13:10:56 -0700

    complete assignment 1.1

---
+++ assignment1/Assignment1_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Assignment1_1 {
+	public static void main(String[] args) {
+    System.out.println("Hello World");
+    Scanner keyboard = new Scanner(System.in);
+    String input =  keyboard.nextLine();
+		System.out.println("Hello " + input);
+	}
+}
