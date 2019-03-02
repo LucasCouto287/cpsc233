5c2f2893bf959bd957dd20196199468693a33d53
Abdullah F. Khan <abdullahkhan0503@gmail.com>
2019-03-01 22:35:42 -0700

    assignment5/IA5: switch to AnchorPanr from Pane

--- assignment5/5.1/BankAccountGUI.java
+++ assignment5/5.1/BankAccountGUI.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.*;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class BankAccountGUI extends Application {
@@ -28,7 +28,7 @@ public class BankAccountGUI extends Application {
     Button withdrawButton = new Button();
     withdrawButton.setText("Withdraw");
 
-    Pane root = new Pane();
+    AnchorPane root = new AnchorPane();
     // styling for all elements (root, account holder heading, balance heading, input field + its label, deposit/withdraw buttons)
     root.setStyle("-fx-background-color: #3C3F45;");
     heading.setStyle("-fx-font-size: 23px; -fx-fill: #FBFBFC");
@@ -40,20 +40,15 @@ public class BankAccountGUI extends Application {
     // set the text field size
     amountInput.setPrefSize(300, 10);
     // positioning for the elements
-    amountInput.setLayoutX(110);
-    amountInput.setLayoutY(140);
-    depositButton.setLayoutX(160);
-    depositButton.setLayoutY(230);
-    withdrawButton.setLayoutX(260);
-    withdrawButton.setLayoutY(230);
+    root.setLeftAnchor(amountInput, 110.0);
+    root.setBottomAnchor(amountInput, 120.0);
+    root.setBottomAnchor(depositButton, 40.0);
+    root.setRightAnchor(depositButton, 260.0);
+    root.setRightAnchor(withdrawButton, 140.0);
+    root.setBottomAnchor(withdrawButton, 40.0);
     // add all elements into the root pane
-    root.getChildren().add(heading);
-    root.getChildren().add(balanceText);
-    root.getChildren().add(inputLabel);
-    root.getChildren().add(amountInput);
-    root.getChildren().add(depositButton);
-    root.getChildren().add(withdrawButton);
-    // disable windo resizing
+    root.getChildren().addAll(heading, balanceText, inputLabel, amountInput, depositButton, withdrawButton);
+    // disable window resizing
     primaryStage.setResizable(false);
     // set window (Scene) with the root pane and set the height and width
     primaryStage.setScene(new Scene(root, 500, 300));
